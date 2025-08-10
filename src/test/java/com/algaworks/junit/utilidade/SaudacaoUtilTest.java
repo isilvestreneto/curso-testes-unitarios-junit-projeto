3c8b91552f5473926d80226e16aaa8a83d17a30a
Ivan Silvestre <isilvestreneto@gmail.com>
2025-08-10 11:08:38 -0300

    2.2 escrevendo primeiro teste

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,4 +1,16 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SaudacaoUtilTest {
-  
+
+    @Test
+    public void saudar() {
+        String saudacao = SaudacaoUtil.saudar(9);
+        System.out.println(saudacao);
+        assertEquals("Bom dia", saudacao);
+        assertTrue(saudacao.equals("Bom dia"));
+    }
 }
