7d2976572a0e35d2cc6917171f315a28d96da5a8
Ivan Silvestre <isilvestreneto@gmail.com>
2025-08-10 15:24:38 -0300

    2.10 ignorando execucao dos testes condicionalmente com assumptions

--- src/test/java/com/algaworks/junit/utilidade/PessoaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/PessoaTest.java
@@ -1,16 +1,19 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PessoaTest {
 
+    //    @Disabled("Desabilitado para demonstração")
     @Test
-    @Disabled("Desabilitado para demonstração")
+    @EnabledIfEnvironmentVariable(named = "ENV", matches = "DEV")
     void assercaoAgrupada() {
+        Assumptions.assumeTrue("PROD".equals(System.getenv("PROD")), () -> "Abortando teste: Não deve ser executado em PROD");
         Pessoa pessoa = new Pessoa("João", "Silva");
         assertAll("Asserções de pessoa",
                 () -> assertEquals("João", pessoa.getNome(), "O nome deve ser João"),
