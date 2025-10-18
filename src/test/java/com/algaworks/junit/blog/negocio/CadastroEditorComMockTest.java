6b5b8d7354450786fae0326806f1273c484dd974
Ivan <isilvestreneto@gmail.com>
2025-10-18 18:20:33 -0300

    4.12 verificando ordem de chamada de metodos

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -49,30 +49,30 @@ class CadastroEditorComMockTest {
     }
 
     @Test
-    void dado_editor_valido_quando_criar_entao_deve_retornar_um_id_de_cadastro() {
+    void Dado_editor_valido_Quando_criar_entao_Deve_retornar_um_id_de_cadastro() {
         Editor resultado = cadastroEditor.criar(editor);
         long esperado = 1L;
         assertEquals(esperado, resultado.getId());
     }
 
     @Test
-    void dado_um_editor_valido_quando_criar_entao_deve_chamar_metodo_salvar_do_armazenamento() {
+    void Dado_um_editor_valido_Quando_criar_entao_Deve_chamar_metodo_salvar_do_armazenamento() {
         cadastroEditor.criar(editor);
         verify(armazenamentoEditor, Mockito.times(1)).salvar(eq(editor));
     }
 
     @Test
-    void dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
+    void Dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_Deve_enviar_email() {
         when(armazenamentoEditor.salvar(editor)).thenThrow(new RuntimeException());
 
-        assertAll("Nao deve enviar email, quando lancar exception no armazenamento",
+        assertAll("Nao Deve enviar email, Quando lancar exception no armazenamento",
                 () -> assertThrows(RuntimeException.class, () -> cadastroEditor.criar(editor)),
                 () -> verify(gerenciadorEnvioEmail, never()).enviarEmail(any())
         );
     }
 
     @Test
-    void dado_um_editor_valido_quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_Deve_enviar_email_com_destino_ao_editor() {
         Editor editorSalvo = cadastroEditor.criar(editor);
         verify(gerenciadorEnvioEmail).enviarEmail(captor.capture());
         Mensagem mensagem = captor.getValue();
@@ -81,7 +81,7 @@ class CadastroEditorComMockTest {
     }
 
     @Test
-    void dado_um_editor_valido_Quando_cadastrar_Entao_deve_verificar_email() {
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_Deve_verificar_email() {
         Editor editorSpy = spy(editor);
         cadastroEditor.criar(editorSpy);
         verify(editorSpy, atLeast(1)).getEmail();
@@ -95,5 +95,15 @@ class CadastroEditorComMockTest {
         Editor editorComMesmoEmail = new Editor(null, "Ivan", "ivan@email.com", BigDecimal.valueOf(30), true);
         cadastroEditor.criar(editor);
         assertThrows(RegraNegocioException.class, () -> cadastroEditor.criar(editorComMesmoEmail));
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Deve_enviar_email_ao_salvar() {
+        cadastroEditor.criar(editor);
+        InOrder inOrder = inOrder(armazenamentoEditor, gerenciadorEnvioEmail);
+        inOrder.verify(armazenamentoEditor, times(1)).salvar(eq(editor));
+        inOrder.verify(gerenciadorEnvioEmail, times(1))
+                .enviarEmail(any(Mensagem.class));
+
     }
 }
